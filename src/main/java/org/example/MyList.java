1f2dda3de38e003cf71e225c130462346b646aa1
Kumissay <kkapammell@gmail.com>
2024-04-21 19:37:51 +0500

    git commit

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,29 @@
 package org.example;
 import java.util.Iterator;
 
-public interface MyList<T> extends Iterable<T> {
+public interface MyList<T> extends Iterable<T> { //generic interface with parameter T
     interface MyIterator<E> extends Iterator<E>{
 
     }
     MyList.MyIterator<T> myIterator();
-    void add(T item);
-    void set(int index, T item);
-    void add(int index, T item);
-    void addFirst(T item);
-    void addLast(T item);
-    T get (int index);
-    T getFirst();
-    T getLast();
-    T remove(int index);
-    void removeFirst();
-    void removeLast();
-    void sort();
-    int indexOf(Object object);
-    int lastIndexOf(Object object);
-    boolean exists(Object object);
-    public Object[] toArray();
-    void clear();
-    int size();
+    void add(T item); //adding an item at the end
+    void set(int index, T item); //setting item at the certain index
+    void add(int index, T item); //adding item at the certain index
+    void addFirst(T item); //adding to the begiining
+    void addLast(T item); //adding to the end
+    T get (int index); //getting access to item at specific index
+    T getFirst(); //getting the first element
+    T getLast(); //getting access to last element
+    T remove(int index); //removing item at the certain index
+    void removeFirst(); //remove the first eleement
+    void removeLast();//removing the last one
+    void sort(); //sorting the list of elements
+    int indexOf(Object object); //will return the index number our element belongs to
+    int lastIndexOf(Object object);//will return the index of the last occurrence of the specific object
+    boolean exists(Object object); //checks if an item exists in the list
+    public Object[] toArray(); //return array that contains all elemenets
+    void clear(); //remove all elements
+    int size(); //return the number of  elements
 
 
 }
